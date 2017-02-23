88e30687db6fbdaf5d7c59774e58b9a1e92d8de1
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2017-02-23 15:07:10 -0500

    Some defaults.

--- lib/flashcards/config.rb
+++ lib/flashcards/config.rb
@@ -19,16 +19,13 @@ module Flashcards
       @data ||= YAML.load_file(@path)
     end
 
-    # {"learning"=>{"es"=>["present", "past", "reflective"]}}
     def language(language_name = nil)
       if language_name.nil? && data['learning'].keys.length == 1
-        data['learning'].keys.first.to_sym
-        OpenStruct.new(
-          name: data['learning'].keys.first.to_sym,
-          tested_forms: data['learning'].values.first)
+        OpenStruct.new(data['learning'].values.first.merge(name: data['learning'].keys.first.to_sym))
+      elsif language_name.nil? && (default_language = data['learning'].find { |_, lang| lang['default'] })
+        OpenStruct.new(default_language[1].merge(name: default_language[0].to_sym))
       elsif language_name && data['learning'].keys.include?(language_name.to_s)
-        OpenStruct.new(name: language_name,
-          tested_forms: data['learning'][language_name.to_s])
+        OpenStruct.new(data['learning'][language_name.to_s].merge(name: language_name))
       elsif language_name && ! data['learning'].keys.include?(language_name.to_s)
         raise "Language #{language_name} is not configured. Your configured languages are: #{data['learning'].keys.inspect}"
       else
--- spec/flashcards/config_spec.rb
+++ spec/flashcards/config_spec.rb
@@ -13,7 +13,7 @@ describe Flashcards::Config do
 
   describe '#data' do
     it 'loads the config' do
-      expect(subject.data).to eql({'learning' => {'es' => ['present', 'past', 'reflective']}})
+      expect(subject.data).to eql({'learning' => {'es' => {'test_me_on' => ['present', 'past', 'reflective']}}})
     end
   end
 
@@ -21,11 +21,17 @@ describe Flashcards::Config do
     context 'with no arguments' do
       it 'returns the language if there is only one' do
         expect(subject.language.name).to eql(:es)
-        expect(subject.language.tested_forms).to eql(['present', 'past', 'reflective'])
+        expect(subject.language.test_me_on).to eql(['present', 'past', 'reflective'])
+      end
+
+      it 'returns the language if there is a default one' do
+        subject = described_class.new('spec/data/flashcards_multiple_default.yml')
+        expect(subject.language.name).to eql(:es)
+        expect(subject.language.test_me_on).to eql(['present', 'past', 'reflective'])
       end
 
       it 'raises an exception if there is more than one language configured' do
-        subject = described_class.new('spec/data/flashcards_multiple.yml')
+        subject = described_class.new('spec/data/flashcards_multiple_no_default.yml')
         expect { subject.language }.to raise_error
       end
     end
@@ -33,7 +39,7 @@ describe Flashcards::Config do
     context 'with the language as the first and only argument' do
       it 'returns the language there is such language configured' do
         expect(subject.language(:es).name).to eql(:es)
-        expect(subject.language(:es).tested_forms).to eql(['present', 'past', 'reflective'])
+        expect(subject.language(:es).test_me_on).to eql(['present', 'past', 'reflective'])
       end
 
       it 'raises an exception if there is no such language configured' do
