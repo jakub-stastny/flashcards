3460e2b97c9d9a2d049a8330970e2891eff8811a
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2017-02-23 13:27:27 -0500

    colourise works!

--- lib/flashcards/core_exts.rb
+++ lib/flashcards/core_exts.rb
@@ -15,12 +15,23 @@ end
 # NOTE: The recursion is to add support for parsing nesting, such as in <green>1<yellow>2</yellow>.</green>
 # TODO: Nesting still doesn't work, i. e. in <green>✔︎ <yellow>Todavía</yellow> is indeed <yellow>still</yellow>. </green>
 # That's because we don't know which scope are we in (what tags are open).
+
+=begin
+<green>Hello <red>world</red>!</green>
+=end
+require 'term/ansicolor'
+
 def colourise(string, options = Hash.new)
+  colours = Object.new.extend(Term::ANSIColor)
+
   result = string.gsub(/<([^>]+)>(.+?)<\/\1>/m) do |match|
     methods = $1.split('.')
     methods.push(:bold) if options[:bold]
-    methods.reduce($2) do |result, method|
-      result.send(method)
+
+    methods.reduce(inner_text = $2) do |result, method|
+      result.sub!(/(<\/[^>]+>)/, "#{colours.send(method)}\\1")
+      "#{result}.#{method}"
+      "#{colours.send(method)}#{result}"
     end
   end
 
