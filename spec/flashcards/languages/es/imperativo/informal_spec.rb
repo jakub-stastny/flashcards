94ea88776529bfad06bcb9b7fc809bae482ac990
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2017-07-10 19:57:44 +0200

    Specs.

---
+++ spec/flashcards/languages/es/imperativo/informal_spec.rb
@@ -0,0 +1,47 @@
+require 'flashcards'
+require 'flashcards/language'
+
+describe 'Informal commands' do
+  let(:spanish) { Flashcards.app.language }
+
+  describe 'affirmative' do
+    let(:hablar) { spanish.verb('hablar') }
+
+    it do
+      # require 'pry'; binding.pry ###
+      expect(hablar.imperativo_positivo.tú).to eql('habla')
+      expect(hablar.imperativo_positivo.vos).to eql('hablá')
+      expect(hablar.imperativo_formal.usted).to eql('hable')
+      expect(hablar.imperativo_negativo.tú).to eql('hables')
+      expect(hablar.imperativo_negativo.vos).to eql('hables')
+      # hablar.imperativo_formal.{usted,nosotros?,ustedes}
+      # hablar.imperativo_positivo.{tú,vos,nosotros,vosotros}
+      # hablar.imperativo_negativo.{tú,vos,nosotros,vosotros}
+      # require 'pry'; binding.pry ###
+    end
+
+  #   it 'is regular' do
+  #     expect(hablar.presente.exception?(:tú)).to be(false)
+  #     expect(hablar.presente.exception?(:vos)).to be(false)
+  #     expect(hablar.presente.exception?(:nosotros)).to be(false)
+  #
+  #     expect(hablar.presente.tú).to eql('habla')
+  #     expect(hablar.presente.vos).to eql('hablxxxx')
+  #     expect(hablar.presente.nosotros).to eql('hablamonos')
+  #   end
+  #
+  #   it 'is irregular' do
+  #     expect(hablar.presente.exception?(:tú)).to be(false) # this won't work, since we're not proxying exceptions.
+  #     expect(hablar.presente.exception?(:vos)).to be(false)
+  #     expect(hablar.presente.exception?(:nosotros)).to be(false)
+  #
+  #     expect(hablar.presente.tú).to eql('habla')
+  #     expect(hablar.presente.vos).to eql('hablxxxx')
+  #     expect(hablar.presente.nosotros).to eql('hablamonos')
+  #   end
+  # end
+  #
+  # describe 'negative' do
+  #   #
+  end
+end
