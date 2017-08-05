0706974996f605a3c4f889d9c66e2b8320155f53
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2017-08-05 15:26:25 +0200

    Fixed has-not-run-today command.

--- lib/flashcards/commands/has-not-run-today.rb
+++ lib/flashcards/commands/has-not-run-today.rb
@@ -7,13 +7,14 @@ module Flashcards
     # EOF
 
     def run
-      flashcards = Flashcards.app.flashcards
+      app = self.get_app(@args[0])
+      flashcards = app.flashcards.map { |flashcard| flashcard.with(app) }
       to_be_reviewed = flashcards.count(&:time_to_review?)
       new_flashcards = flashcards.count(&:new?)
 
       exit 1 if (to_be_reviewed + new_flashcards) == 0
 
-      last_review_at = flashcards.map { |f| f.correct_answers.values.flatten.sort.last }.compact.sort.last
+      last_review_at = app.flashcards.map { |f| f.correct_answers.values.flatten.sort.last }.compact.sort.last
 
       run_today = last_review_at && last_review_at.to_date == Date.today
       exit 1 if run_today
