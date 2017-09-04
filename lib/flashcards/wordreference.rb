59d28ef058dfde70e4a2da4a76a5270fbc093350
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2017-09-04 18:20:13 +0200

    Automated commit from 04/09/2017.

--- lib/flashcards/wordreference.rb
+++ lib/flashcards/wordreference.rb
@@ -13,7 +13,8 @@ module Flashcards
     end
 
     def self.run(app, all_flashcards)
-      RR::CachedHttp.offline = true
+      # TODO: Change to .prefer_offline.
+      # RR::CachedHttp.offline = true
       RR::CachedHttp.cache_dir = 'tmp/cache' # if $DEBUG or sth ...
       flashcards = self.unverified_verbs(all_flashcards)
 
@@ -23,7 +24,7 @@ module Flashcards
           checker.run
           if checker.correct?
             puts "<green>✔︎</green> #{flashcard.expressions.first}".colourise(bold: true)
-            flashcard.set_checksum
+            flashcard.with(app).set_checksum
           else
             puts "<red>✘</red> #{flashcard.expressions.first}".colourise(bold: true)
             puts checker.warnings.map { |warning| "- #{warning}" }
@@ -43,7 +44,6 @@ module Flashcards
     end
 
     def url
-      require 'pry'; binding.pry ###
       asciified_infinitive = @flashcard.with(@app).verb.infinitive.tr('ñ', 'n')
       "http://www.wordreference.com/conj/ESverbs.aspx?v=#{asciified_infinitive}"
     end
@@ -95,18 +95,22 @@ module Flashcards
         imperativo_positivo: ['afirmativo', 0],
         imperativo_negativo: ['negativo', 0]
       }.each do |flashcards_tense_name, (wr_tense_name, index)|
-        results = conjugations(groups, wr_tense_name, index)[1..-1].map { |word| word.sub(/^(no )?(\S+)\!$/, '\2') }
-        tense = @flashcard.with(@app).verb.send(flashcards_tense_name)
-        test("1 #{@flashcard.with(@app).verb.infinitive}.#{tense.tense}.tú", results[0], tense.tú)
-        test("1 #{@flashcard.with(@app).verb.infinitive}.#{tense.tense}.vos", process_vos(results[5]), tense.vos)
-        test("1 #{@flashcard.with(@app).verb.infinitive}.#{tense.tense}.nosotros", results[2], tense.nosotros)
-        test("1 #{@flashcard.with(@app).verb.infinitive}.#{tense.tense}.vosotros", results[3], tense.vosotros)
+        results = (conjugations(groups, wr_tense_name, index)[1..-1] || Array.new).map { |word| word.sub(/^(no )?(\S+)\!$/, '\2') }
+        unless results.empty?
+          tense = @flashcard.with(@app).verb.send(flashcards_tense_name)
+          test("1 #{@flashcard.with(@app).verb.infinitive}.#{tense.tense}.tú", results[0], tense.tú)
+          test("1 #{@flashcard.with(@app).verb.infinitive}.#{tense.tense}.vos", process_vos(results[5]), tense.vos)
+          test("1 #{@flashcard.with(@app).verb.infinitive}.#{tense.tense}.nosotros", results[2], tense.nosotros)
+          test("1 #{@flashcard.with(@app).verb.infinitive}.#{tense.tense}.vosotros", results[3], tense.vosotros)
+        else
+          @warnings << "Empty results (bug)."
+        end
       end
 
       {
         imperativo_formal: ['negativo', 0]
       }.each do |flashcards_tense_name, (wr_tense_name, index)|
-        results = conjugations(groups, wr_tense_name, index)[1..-1].map { |word| word.sub(/^(no )?(\S+)\!$/, '\2') }
+        results = (conjugations(groups, wr_tense_name, index)[1..-1] || Array.new).map { |word| word.sub(/^(no )?(\S+)\!$/, '\2') }
         tense = @flashcard.with(@app).verb.send(flashcards_tense_name)
         test("2 #{@flashcard.with(@app).verb.infinitive}.#{tense.tense}.usted", results[1], tense.usted)
         test("2 #{@flashcard.with(@app).verb.infinitive}.#{tense.tense}.ustedes", results[4], tense.ustedes)
@@ -115,7 +119,7 @@ module Flashcards
       {
         imperativo_formal: ['negativo', 0] # same as above, but hash can't have two same keys ...
       }.each do |flashcards_tense_name, (wr_tense_name, index)|
-        results = conjugations(groups, wr_tense_name, index)[1..-1].map { |word| word.sub(/^(no )?(\S+)\!$/, '\2') }
+        results = (conjugations(groups, wr_tense_name, index)[1..-1] || Array.new).map { |word| word.sub(/^(no )?(\S+)\!$/, '\2') }
         tense = @flashcard.with(@app).verb.send(flashcards_tense_name)
         test("3 #{@flashcard.with(@app).verb.infinitive}.#{tense.tense}.usted", results[1], tense.usted)
         test("3 #{@flashcard.with(@app).verb.infinitive}.#{tense.tense}.ustedes", results[4], tense.ustedes)
@@ -125,6 +129,7 @@ module Flashcards
 
     def conjugations(groups, label, index)
       tense = groups.select { |group| group.css('tr')[0].text == label }[index]
+      return Array.new if tense.nil?
       tense.children[1..-1].map { |tr| tr.css('td')[0].text } # th is the pronoun
     end
 
@@ -137,7 +142,7 @@ module Flashcards
     end
 
     def process_vos(list_or_text) # TODO: refactor this, why can it be an array?
-      [list_or_text].flatten.map do |word|
+      [list_or_text].flatten.compact.map do |word|
         word.delete('*!').sub(/no /, '').split(/,\s*/)
       end.flatten
     end
