583eed054711c4c925a248b02dcd11cc52285450
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2017-10-12 14:41:19 +0200

    Fix the add command.

--- lib/flashcards/commands/add.rb
+++ lib/flashcards/commands/add.rb
@@ -1,5 +1,6 @@
 require 'flashcards/command'
 require 'flashcards/utils'
+require 'refined-refinements/colours'
 
   # <magenta>Multilingual mode</magenta> If you have more than one language configured in your
   #   <bright_black>~/.config/flashcards.yml</bright_black>, you can specify which one you want to add the
@@ -11,6 +12,9 @@ require 'flashcards/utils'
   #   --no-edit #verb
 module Flashcards
   class AddCommand < SingleLanguageCommand
+    using RR::ColourExts
+    using CoreExts
+
     self.help = <<-EOF
       #{self.main_command} <red>add</red> [word] [translations]
       #{self.main_command} <red>add</red> todavía still
@@ -20,7 +24,7 @@ module Flashcards
     EOF
 
     def process_arguments
-      args = self.get_args(@args)
+      app, args = self.get_args(@args)
       args = args.group_by do |item|
         case item
         when /^-/ then :args
@@ -29,11 +33,11 @@ module Flashcards
       end
 
       args.default_proc = Proc.new { |hash, key| hash[key] = Array.new }
-      args
+      return app, args
     end
 
-    def matching_flashcards(expressions, translations)
-      flashcards = Flashcards.app.flashcards # !
+    def matching_flashcards(app, expressions, translations)
+      flashcards = app.flashcards # !
 
       flashcards.select do |flashcard|
         ! (flashcard.expressions & expressions).empty? &&
@@ -42,12 +46,12 @@ module Flashcards
     end
 
     def run
-      args = self.process_arguments
+      app, args = self.process_arguments
 
       if args[:values].empty?
-        self.run_interactive
+        self.run_interactive(app, args)
       elsif args[:values].length == 1
-        matching_flashcards = self.matching_flashcards(args[:values][0].split(/,\s*/), Array.new)
+        matching_flashcards = self.matching_flashcards(app, args[:values][0].split(/,\s*/), Array.new)
 
         if matching_flashcards.empty?
           puts "There is no definition for <yellow>#{args[:values][0]}</yellow> yet.".colourise
@@ -60,43 +64,47 @@ module Flashcards
           puts "<magenta>Matching flashcards:</magenta>\n- #{matching_flashcards_text}".colourise
         end
       elsif args[:values].length == 2
-        flashcards = Flashcards.app.flashcards
-
-        expressions  = args[:values][-2].split(/,\s*/)
-        translations = args[:values][-1].split(/,\s*/)
-
-        flashcards.each do |flashcard|
-          if (! (flashcard.expressions & expressions).empty?) && (! (flashcard.translations & translations).empty?)
-            abort "Same flashcard: #{flashcard}.".colourise
-          end
-        end
-
-        flashcard = self.sample_flashcard(args)
-
-        if args[:args].include?('--no-edit')
-          flashcard.examples.clear
-          flashcard.metadata.clear
-          flashcard.tags << :new
-          flashcards << flashcard
-          flashcards.save
-        else
-          if flashcard = self.edit_flashcard(flashcard)
-            flashcards << flashcard
-            flashcards.save
-          else
-            abort "No data found."
-          end
-        end
+        self.add(app, args)
       else
         # TODO: Commander::HELP_ITEMS[:add]
         abort "Usage: #{File.basename($0)} [lang] [word] [translation] [tags]"
       end
     end
 
-    def sample_flashcard
+    def add(app, args)
+      flashcards = app.flashcards
+
+      expressions  = args[:values][-2].split(/,\s*/)
+      translations = args[:values][-1].split(/,\s*/)
+
+      flashcards.each do |flashcard|
+        if (! (flashcard.expressions & expressions).empty?) && (! (flashcard.translations & translations).empty?)
+          abort "Same flashcard: #{flashcard}.".colourise
+        end
+      end
+
+      flashcard = self.sample_flashcard(args)
+
+      if args[:args].include?('--no-edit')
+        flashcard.examples.clear
+        flashcard.metadata.clear
+        flashcard.tags << :new
+        flashcards << flashcard
+        flashcards.save
+      else
+        if flashcard = self.edit_flashcard(flashcard)
+          flashcards << flashcard
+          flashcards.save
+        else
+          abort "No data found."
+        end
+      end
+    end
+
+    def sample_flashcard(args)
       Flashcard.new(
-        expressions: expressions,
-        translations: translations,
+        expressions: args[:values][0].split(/,\s*/),
+        translations: args[:values][1].split(/,\s*/),
         tags: (args[:tags] || Array.new).map { |tag| tag[1..-1].to_sym },
         examples: [
           Example.new(expression: 'Expression 1.', translation: 'Translation 1.'),
@@ -108,7 +116,7 @@ module Flashcards
       )
     end
 
-    def run_interactive
+    def run_interactive(app, args)
       puts "<blue.bold>Usage</blue.bold>: <green>expression 1, expression 2</green> <magenta>=</magenta> <green>translation 1, translation 2</green> <yellow>#tags</yellow>\n\n".colourise
       puts "Press <bold>Esc</bold> for the command mode.\n\n".colourise
 
@@ -126,10 +134,10 @@ module Flashcards
             puts "~ Flashcard #{matching_flashcards.join_with_and(&:to_s)} already exists.\n\n".colourise
           end
         elsif values.length == 2
-          matching_flashcards = self.matching_flashcards(values[0].split(/,\s*/), values[1].split(/,\s*/))
+          matching_flashcards = self.matching_flashcards(app, values[0].split(/,\s*/), values[1].split(/,\s*/))
           if matching_flashcards.empty?
             puts "~ Adding <green>#{values.inspect}</green>#{" with tags #{tags.join_with_and { |tag| "<yellow>#{tag}</yellow>" }}" unless tags.empty?}.\n\n".colourise
-            self.add([values, tags, '--no-edit'].flatten)
+            self.add(app, values: values, tags: tags, args: '--no-edit') # Invoke the command in a non-interactive mode.
           else
             puts "~ Flashcard #{matching_flashcards.join_with_and(&:to_s)} already exists.\n\n".colourise
           end
@@ -137,6 +145,8 @@ module Flashcards
           puts "<red>!</red> Invalid input, try again.".colourise
         end
       end
+    rescue Interrupt
+      puts; exit
     end
   end
 end
