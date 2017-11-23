3cf2123fe7dc3db8834453b1f41d532ff31363c5
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2017-11-23 04:56:48 +0100

    Status line implemented.

--- lib/flashcards/commands/add-interactive.rb
+++ lib/flashcards/commands/add-interactive.rb
@@ -16,7 +16,8 @@ module Flashcards
     def run
       ::App.new.run do |app, window|
         window.write("<blue.bold>Usage</blue.bold>: <green>expression 1, expression 2</green> <magenta>=</magenta> <green>translation 1, translation 2</green> <yellow>#tags</yellow>\n\n")
-        window.write("Press <bold>Esc</bold> for the command mode.\n\n")
+        window.write("Use <bold>arrows</bold> to access items from the history.\n")
+        window.write("Press <bold>Esc</bold> for the command mode, once an item has been added.\n\n")
         window.refresh
 
         line = app.readline("> ")
@@ -31,27 +32,30 @@ module Flashcards
           @log.puts('A'); @log.flush ####
           matching_flashcards = Utils.matching_flashcards(@app.flashcards, values[0].split(/,\s*/))
           if matching_flashcards.empty?
-            window.write("~ There is no definition yet.")
+            set_status_line(window, "There is no definition yet.")
           else
-            window.write("~ Flashcard #{matching_flashcards.join_with_and(&:to_s)} already exists.")
+            set_status_line(window, "Flashcard #{matching_flashcards.join_with_and(&:to_s)} already exists.")
           end
-          window.refresh; sleep 1 ### TODO: Write into a status line instead.
         elsif values.length == 2
           @log.puts('B'); @log.flush ####
           matching_flashcards = Utils.matching_flashcards(@app.flashcards, values[0].split(/,\s*/), values[1].split(/,\s*/))
           if matching_flashcards.empty?
-            window.write("~ Adding <green>#{values.inspect}</green>#{" with tags #{tags.join_with_and { |tag| "<yellow>#{tag}</yellow>" }}" unless tags.empty?}.")
+            set_status_line(window, "Adding <green>#{values.inspect}</green>#{" with tags #{tags.join_with_and { |tag| "<yellow>#{tag}</yellow>" }}" unless tags.empty?}.")
             @command.add(@app, values: values, tags: tags, args: '--no-edit') # Invoke the command in a non-interactive mode.
           else
-            window.write("~ Flashcard #{matching_flashcards.join_with_and(&:to_s)} already exists.")
+            set_status_line(window, "Flashcard #{matching_flashcards.join_with_and(&:to_s)} already exists.")
           end
-          window.refresh; sleep 1 ### TODO: Write into a status line instead.
         else
           @log.puts('C'); @log.flush ####
-          window.write("<red>!</red> Invalid input, try again.")
-          window.refresh; sleep 1 ### TODO: Write into a status line instead.
+          set_status_line(window, "<red>Invalid input, try again.</red>")
         end
       end
+    end
+
+    def set_status_line(window, text)
+      window.setpos(Curses.lines - 1, 0)
+      window.write(text)
+      window.refresh; sleep 1.5
     end
   end
 end
